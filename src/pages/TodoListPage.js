4e64c64ebcb36dd9ce0b64a0d414d1a54d385c56
Limgyuho <radum0719@hanmail.net>
2023-06-05 21:35:06 +0900

    나머지 정렬 기능들 구현

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -3,8 +3,11 @@ import { Tab, Tabs } from "@mui/material";
 import { useTodosState, useTodoOptionDrawerState } from "../hooks";
 import TodoOptionDrawer from "../components/TodoOptionDrawer";
 import TodoListItem from "../components/TodoListItem";
-import { TodoList__filterCompletedIndexAtom,TodoList__sortIndexAtom } from "../atoms";
-import { useRecoilState } from "recoil";
+import {
+  TodoList__filterCompletedIndexAtom,
+  TodoList__sortIndexAtom,
+} from "../atoms";
+import { useRecoilState, useResetRecoilState } from "recoil";
 
 export default function TodoList() {
   const todosState = useTodosState();
@@ -13,7 +16,8 @@ export default function TodoList() {
   const [filterCompletedIndex, setFilterCompletedIndex] = useRecoilState(
     TodoList__filterCompletedIndexAtom
   );
-    const [sortIndex, setSortIndex] = useRecoilState(TodoList__sortIndexAtom);
+
+  const [sortIndex, setSortIndex] = useRecoilState(TodoList__sortIndexAtom);
 
   const getFilteredTodos = () => {
     if (filterCompletedIndex == 1) {
@@ -29,11 +33,31 @@ export default function TodoList() {
 
   const filteredTodos = getFilteredTodos();
 
-  const sortedTodos = [...filteredTodos].sort((a, b) => {
-    if (a.performDate == b.performDate) return 0;
+  const getSortedTodos = () => {
+    if (sortIndex == 0) {
+      return [...filteredTodos].sort((a, b) => {
+        if (a.performDate == b.performDate) return 0;
 
-     return a.performDate > b.performDate ? 1 : -1;
-  })
+        return a.performDate > b.performDate ? 1 : -1;
+      });
+    } else if (sortIndex == 1) {
+      return [...filteredTodos].sort((a, b) => {
+        if (a.performDate == b.performDate) return 0;
+
+        return a.performDate < b.performDate ? 1 : -1;
+      });
+    } else if (sortIndex == 2) {
+      return [...filteredTodos].sort((a, b) => {
+        if (a.id == b.id) return 0;
+
+        return a.id > b.id ? 1 : -1;
+      });
+    }
+
+    return filteredTodos;
+  };
+
+  const sortedTodos = getSortedTodos();
 
   return (
     <>
@@ -42,7 +66,7 @@ export default function TodoList() {
       <Tabs
         variant="fullWidth"
         value={filterCompletedIndex}
-        onChange={(event, newValue) => setSortIndex(newValue)}
+        onChange={(event, newValue) => setFilterCompletedIndex(newValue)}
       >
         <Tab
           label={
@@ -73,69 +97,63 @@ export default function TodoList() {
         />
       </Tabs>
 
-
-      
-
-
-
-
       <Tabs
-        variant="scrollbar"
+        variant="scrollable"
         value={sortIndex}
-        onChange={(event, newValue) => setFilterCompletedIndex(newValue)}
+        onChange={(event, newValue) => setSortIndex(newValue)}
       >
         <Tab
           className="flex-grow !max-w-[none] px-4"
           label={
             <span className="flex items-baseline">
-              <i class="fa-solid fa-clock"></i>
-              <span className="mx-2 whitespace-nowrap">급해요</span>
-              <i class="fa-solid fa-sort-up relative top-[3px] ml-1"></i>
+              <i className="fa-regular fa-clock"></i>
+              <span className="mx-2 whitespave-nowrap">급해요</span>
+              <i className="fa-solid fa-sort-up relative top-[3px]"></i>
             </span>
           }
           value={0}
         />
+
         <Tab
           className="flex-grow !max-w-[none] px-4"
           label={
             <span className="flex items-baseline">
-              <i class="fa-solid fa-clock"></i>
-              <span className="mx-2 whitespace-nowrap">널널해요</span>
-              <i class="fa-solid fa-sort-down relative top-[3px] ml-1"></i>
+              <i className="fa-regular fa-clock"></i>
+              <span className="mx-2 whitespave-nowrap">널널해요</span>
+              <i className="fa-solid fa-sort-down relative top-[-3px]"></i>
             </span>
           }
           value={1}
         />
+
         <Tab
           className="flex-grow !max-w-[none] px-4"
           label={
             <span className="flex items-baseline">
-              <i class="fa-solid fa-pencil"></i>
-              <span className="mx-2 whitespace-nowrap">작성순</span>
-              <i class="fa-solid fa-sort-up relative top-[3px] ml-1"></i>
+              <i className="fa-solid fa-pencil"></i>
+              <span className="mx-2 whitespave-nowrap">작성순</span>
+              <i className="fa-solid fa-sort-up relative top-[3px]"></i>
             </span>
           }
           value={2}
         />
+
         <Tab
           className="flex-grow !max-w-[none] px-4"
           label={
             <span className="flex items-baseline">
-              <i class="fa-solid fa-pencil"></i>
-              <span className="mx-2 whitespace-nowrap">작성순</span>
-              <i class="fa-solid fa-sort-down relative top-[3px] ml-1"></i>
+              <i className="fa-solid fa-pencil"></i>
+              <span className="mx-2 whitespave-nowrap">작성순</span>
+              <i className="fa-solid fa-sort-down relative top-[-3px]"></i>
             </span>
           }
           value={3}
         />
- 
- 
- 
       </Tabs>
 
       <div className="mt-4 px-4">
         <ul>
-          {sortedTodos .map((todo, index) => (
+          {sortedTodos.map((todo, index) => (
             <TodoListItem
               key={todo.id}
               todo={todo}
